855727cb0d8282ae1f695d1ff7c4754e8a9d96c2
zevav <zev@averba.ch>
2019-11-14 11:03:19 +0100

    ...

---
+++ services/users/manage.py
@@ -0,0 +1,66 @@
+import sys
+import unittest
+
+import coverage
+from flask.cli import FlaskGroup
+
+from project import create_app, db
+from project.api.models import User
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py',
+    ]
+)
+COV.start()
+
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
+
+
+@cli.command('recreate_db')
+def recreate_all():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+
+@cli.command('seed_db')
+def seed_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+    db.session.add(User(username='michael', email='hermanmu@gmail.com'))
+    db.session.add(User(username='michaelherman', email='michael@herman.org'))
+    db.session.commit()
+
+
+@cli.command()
+def test():
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    sys.exit(result)
+
+
+@cli.command()
+def cov():
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    sys.exit(result)
+
+
+if __name__ == "__main__":
+    cli()
---
+++ services/users/project/api/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import func
+
+from project import db
+
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    active = db.Column(db.Boolean, default=True, nullable=False)
+    created_date = db.Column(db.DateTime, default=func.now(), nullable=False)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'active': self.active,
+        }
