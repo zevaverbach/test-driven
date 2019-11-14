19b5488d22fb1666bc2db5aaad099693f70069b6
zevav <zev@averba.ch>
2019-11-14 16:16:31 +0100

    lots of things

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,17 +1,23 @@
 import os
 
 from flask import Flask
+from flask_cors import CORS
+from flask_debugtoolbar import DebugToolbarExtension
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+toolbar = DebugToolbarExtension()
+cors = CORS()
 
 
-def create_app(script_info=None):
+def create_app():
     app = Flask(__name__)
     app_settings = os.getenv('APP_SETTINGS')
     app.config.from_object(app_settings)
 
     db.init_app(app)
+    toolbar.init_app(app)
+    cors.init_app(app)
 
     from project.api.users import users_blueprint
     app.register_blueprint(users_blueprint)
